c5cfc50231de5d00247f5dd8a2ae6f7c390a9b8a
amanhere-01 <amanhere0101@gmail.com>
2024-07-30 23:02:36 +0530

    core

--- lib/core/common/widgets/loader.dart
+++ lib/core/common/widgets/loader.dart
@@ -8,7 +8,7 @@ class Loader extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: CircularProgressIndicator(),
+      child: CircularProgressIndicator(color: Colors.white,),
     );
   }
 
---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  static const List<String> topicsList =[
+    'Technology',
+    'Business',
+    'Programming',
+    'Entertainment',
+    'Education',
+    'Health'
+  ];
+}
