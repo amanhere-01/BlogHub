6b2374d8c2a7dadfda7d99d78de368304305b80c
amanhere-01 <amanhere0101@gmail.com>
2024-08-01 00:21:35 +0530

    register dependencies

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:blog_hub/core/common/cubits/app_user/app_user_cubit.dart';
+import 'package:blog_hub/core/network/connection_checker.dart';
 import 'package:blog_hub/features/auth/domain/repository/auth_repository.dart';
 import 'package:blog_hub/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_hub/features/auth/domain/usecases/user_signin.dart';
@@ -11,6 +12,7 @@ import 'package:blog_hub/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:blog_hub/features/blog/domain/usecases/upload_blog.dart';
 import 'package:blog_hub/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/secret/app_secrets.dart';
@@ -25,9 +27,13 @@ Future<void> initDependencies() async {
   final supabase = await Supabase.initialize(
       url: AppSecrets.supabaseUrl, anonKey: AppSecrets.supabaseAnonKey);
   serviceLocator.registerLazySingleton(() => supabase.client);
+  serviceLocator.registerFactory(() => InternetConnection() );
 
   //core
   serviceLocator.registerLazySingleton(() => AppUserCubit());
+  serviceLocator.registerFactory<ConnectionChecker>(
+          () => ConnectionCheckerImpl(serviceLocator())
+  );
 }
 
 void _initAuth() {
@@ -40,7 +46,7 @@ void _initAuth() {
 
   // Repository
   serviceLocator.registerFactory<AuthRepository>(
-    () => AuthRepositoryImplementation(serviceLocator()),
+    () => AuthRepositoryImplementation(serviceLocator(), serviceLocator()),
   );
 
   // Use Cases
